321698065787aa749514e0005e8f8a5728eaf272
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-09-12 11:42:32 +0300

    sql alchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,40 @@
 from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+def create_table(base , engine):
+    # You already passed base, no need to redefine
+      base.metadata.create_all(engine)
+   
 
-def create_table(base):
-    pass
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+
+    return dogs 
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter_by(name=name).first()  
+    return dog
+
+    
 
 def find_by_id(session, id):
-    pass
+   dog = session.query(Dog).filter_by(id=id).first()  
+   return dog
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter_by(name=name, breed=breed).first()
+    # dog = session.query(Dog).filter_by(name=name) , (breed=breed).first()  
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed  
+    session.commit()
+
+    # dog = session.query(Dog).filter_by(dog=dog, breed=breed).first()
+    # return dog
